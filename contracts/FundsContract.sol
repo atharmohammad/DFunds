4979df6fdeaa9dea26361a97fd8f1fee7c53d93d
atharmohammad <mohd.rule123@gmail.com>
2022-02-26 16:22:38 +0530

    storing the donator

--- contracts/FundsContract.sol
+++ contracts/FundsContract.sol
@@ -2,6 +2,9 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Funds{
+    address[] public donators ;
     receive() external payable{}
-    function addFunds() external payable{}
+    function addFunds() external payable{
+        donators.push(msg.sender);
+    }
 }
