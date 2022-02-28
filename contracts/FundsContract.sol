90f33b1e5e4c255f1f46bc3b590cc92081bc7569
atharmohammad <mohd.rule123@gmail.com>
2022-02-28 15:22:02 +0530

    get donators without repitions

--- contracts/FundsContract.sol
+++ contracts/FundsContract.sol
@@ -2,9 +2,23 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Funds{
-    address[] public donators ;
+    mapping(address => bool) addedDonors;
+    mapping(uint => address) index_to_donor;
+
+    uint8 numberOfDonors;
     receive() external payable{}
     function addFunds() external payable{
-        donators.push(msg.sender);
+        if(addedDonors[msg.sender] == false){
+            addedDonors[msg.sender] = true;
+            index_to_donor[numberOfDonors] = msg.sender;
+            numberOfDonors++;
+        }
+    }
+    function getDonators() external view returns (address[] memory){
+        address [] memory donators = new address [](numberOfDonors);
+        for(uint i = 0; i<numberOfDonors; i++){
+            donators[i] = index_to_donor[i];
+        }
+        return donators;
     }
 }
